24fb3ee147102b1ebb77e17a71eed5851b0e25bc
Juan Bautista <jbautistapulido@gmail.com>
2025-05-21 18:21:40 -0500

    Working on the weather api

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,8 +1,24 @@
 import React, { useEffect, useState } from 'react'
 import { Card } from './components/ui/card';
 
+interface WeatherData {
+    location: {
+        name: string;
+        country: string;
+    };
+    current: {
+        temp_c: number;
+        humidity: number;
+        condition: {
+            text: string;
+            icon: string
+        };
+    };
+}
+
+
 function App() {
-    const [data, setData] = useState([]);
+    const [data, setData] = useState<WeatherData | null>(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
@@ -21,22 +37,21 @@ function App() {
         <div className='container'>
             <h1>Aplicacion de clima</h1>
             {
-                loading ? (
+                loading || !data ? (
                     <h1>Loading</h1>
                 ) : (
-                    (
-                        <div className="container-weather">
-                            <h2>{data.location.name}, {data.location.country}</h2>
-                            <p><strong>Temperatura:</strong> {data.current.temp_c}°C</p>
-                            <p><strong>Clima:</strong> {data.current.condition.text}</p>
-                            <img src={data.current.condition.icon} alt={data.current.condition.text} />
-                            <p><strong>Humedad:</strong> {data.current.humidity}%</p>
-                        </div>
-                    )
+                    <div className="container-weather">
+                        <h2>{data.location.name}, {data.location.country}</h2>
+                        <p><strong>Temperatura:</strong> {data.current.temp_c}°C</p>
+                        <p><strong>Clima:</strong> {data.current.condition.text}</p>
+                        <img src={data.current.condition.icon} alt={data.current.condition.text} />
+                        <p><strong>Humedad:</strong> {data.current.humidity}%</p>
+                    </div>
                 )
             }
         </div>
-    )
+    );
+
 }
 
 export default App;
