23caf6277c4461a71be2e602b0238236cfd009b0
Juan Bautista <jbautistapulido@gmail.com>
2025-05-22 17:46:45 -0500

    The trello repo is now added

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./components/ui/card";
+
+interface WeatherData {
+
+}
 
 function App() {
+  const [data, setData] = useState<WeatherData | null>(null)
+  const [loading, setLoading] = useState(true)
+
+
+  useEffect(() => {
+    async function FetchData() {
+      const response = await fetch('http://api.weatherapi.com/v1/current.json?key=12abee8958704a97a46221338252704&q=London&aqi=no')
+      const data = await response.json()
+      setData(data)
+      setLoading(false)
+      console.log(data.location)
+
+    }
+
+    FetchData();
+  }, [])
+
   return (
-    <div>App</div>
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>El titulo de la card</CardTitle>
+        <CardDescription>Aplicación de temperatura</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Información</p>
+      </CardContent>
+      <CardFooter>
+
+      </CardFooter>
+    </Card>
   )
 }
 
