10689cf75b7b3773d6372dcd85cce383eed39c4e
Juan Bautista <jbautistapulido@gmail.com>
2025-05-29 12:50:49 -0500

    The weather app is working

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./components/ui/card";
+import { Button } from "./components/ui/button";
 
 interface WeatherData {
   location: {
@@ -16,19 +17,28 @@ interface WeatherData {
   }
 }
 
+const content = [
+  {
+    title: "Weather app",
+    description: "Enter the city to find out its weather",
+  }
+]
+
 function App() {
   const [city, setCity] = useState("London");
   const [data, setData] = useState<WeatherData | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
-  const fetchData = async () => {
+    const fetchData = async () => {
     setLoading(true);
     setError("");
 
     try {
       const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=12abee8958704a97a46221338252704&q=${city}&aqi=no`)
+      
       if (!response.ok) throw new Error("City not found");
+      
       const data = await response.json()
       setData(data);
       console.log(data)
@@ -37,41 +47,51 @@ function App() {
       setError(err.message)
       console.log(error)
       setData(null)
+    } finally {
+      setLoading(false);
+
     }
 
   }
 
+
   return (
-    <div className="">
-      <Card className="w-[350px]">
+    <div className="min-h-screen flex items-center justify-center bg-[#e8e8e8] p-6">
+      <Card className="w-[350px] bg-white">
         <CardHeader>
-          <CardTitle>Clima actual</CardTitle>
-          <CardDescription>Ingresa la ciudad</CardDescription>
+          <CardTitle>Weather app</CardTitle>
+          <CardDescription>Enter the city to find out its weather</CardDescription>
         </CardHeader>
         <CardContent>
           <input type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder="Ingresa una ciudad" className="w-full p-2 border border-gray-300 rounded-md" />
-          <button onClick={fetchData} className="bg-gray-100 text-gray-800 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" >Buscar Clima</button>
         </CardContent>
-        <CardFooter>
-          <button onClick={fetchData}>Buscar Data</button>
+        <CardFooter className="flex justify-center">
+          <Button
+            onClick={fetchData}
+            className="rounded-lg bg-purple-600 px-5 py-3 text-sm font-medium text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 hover:bg-purple-700 transition"
+          >
+            Buscar clima
+          </Button>
         </CardFooter>
-      </Card>
-      {data && (
-        <div className="text-center">
-          <h3 className="text-xl font-bold">{data.location.name}, {data.location.country}</h3>
-          <p>{data.location.localtime}</p>
-          <img
-            src={`https:${data.current.condition.icon}`}
-            alt={data.current.condition.text}
-            className="mx-auto"
-          />
-          <p className="text-2xl font-bold">{data.current.temp_c}°C</p>
-          <p>{data.current.condition.text}</p>
-        </div>
-      )
+        {loading && <p className="text-center">Cargando....</p> }
+        {error && <p className="text-red-600 text-center">{error}</p>}
 
-      }
-      <CardFooter/>
+        {data && (
+          <div className="text-center">
+            <h3 className="text-xl font-bold">{data.location.name}, {data.location.country}</h3>
+            <p>{data.location.localtime}</p>
+            <img
+              src={`https:${data.current.condition.icon}`}
+              alt={data.current.condition.text}
+              className="mx-auto"
+            />
+            <p className="text-2xl font-bold">{data.current.temp_c}°C</p>
+            <p>{data.current.condition.text}</p>
+          </div>
+        )
+
+        }
+      </Card>
     </div>
 
   )
